a06e19e8cb9837d34e108367c0c3646c08013ba1
ArekFrel <arkadiuszfrel@gmail.com>
2023-10-12 08:28:17 +0200

    sat - const changes

--- const.py
+++ const.py
@@ -21,7 +21,7 @@ ACC_EXT = [
 """Number of second to wait before new refreshing"""
 TIME_OF_BREAK = 120
 
-"""Number of second after which the catalog is moved."""
+"""Number of second after which the catalog is ready to be moved."""
 TIMEOUT_FOR_PLANERS = 1
 
 """ Time between the script is not running"""
@@ -31,10 +31,11 @@ FROM_OCLOCK = 1
 TO_OCLOCK = 6
 
 """ PRODUCTION - catalogs where drawings are stored. """
-PRODUCTION = 'W:/!!__PRODUKCJA__!!/1__Rysunki/'
+# PRODUCTION = 'W:/!!__PRODUKCJA__!!/1__Rysunki/'
+PRODUCTION = 'C:/Dokumenty/sat/1__Rysunki/'
 
 """ START_CATALOG - catalog where new drawing are uploaded by planners."""
-START_CATALOG = 'W:/!!__PRODUKCJA__!!/4__Nowe_Rysunki/'
+START_CATALOG = 'C:/Dokumenty/sat/4__Nowe_Rysunki/'
 
 """ RAPORT_CATALOG - catalog where Sap report are stored."""
 RAPORT_CATALOG = 'W:/!!__PRODUKCJA__!!/2__Baza_Danych/'
