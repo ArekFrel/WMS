25bd077f4116405dd8e297e22c08833dc9ba4158
ArekFrel <arkadiuszfrel@gmail.com>
2023-10-12 15:12:56 +0200

    merger 1

---
+++ merger.py
@@ -0,0 +1,24 @@
+import os
+import fitz
+from stat import S_IWRITE
+catalog = 'C:\Dokumenty\sat'
+
+for file in os.listdir(catalog):
+
+    if not file.lower().endswith('.pdf'):
+        continue
+    doc_path = os.path.join(catalog, file)
+    merged_doc = os.path.join(catalog, 'merged.pdf')
+    save_doc = os.path.join(catalog, 'merged_temp.pdf')
+    with fitz.open(doc_path) as doc:
+        if 'merged.pdf' not in os.listdir(catalog):
+            doc.save(merged_doc)
+        else:
+            with fitz.open(merged_doc) as doc_merged:
+                doc_merged.insert_file(doc)
+                doc_merged.save(save_doc)
+                os.chmod(merged_doc, S_IWRITE)
+    os.remove(merged_doc)
+    os.rename(save_doc, merged_doc)
+
+
